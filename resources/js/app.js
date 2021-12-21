4814fba31b58b5625578d7eff57d0ea7d64bae2a
Mohit Baldawa <mohitbaldawa@gmail.com>
2021-12-21 10:52:34 +0530

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,3 @@
-/* ---------------------------------------------- */
-/*            CODE EXPLAINED TUTORIALS            */
-/*         www.youtube.com/CodeExplained          */
-/* ---------------------------------------------- */
 
 // SELECT ALL ELEMENTS
 const country_name_element = document.querySelector(".country .name");
